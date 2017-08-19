bf4dc879400101abb9f08e6607baf461f41e5a39
mnavinc <naveen@tententen.in>
2017-08-19 18:35:45 +0530

    new task

--- static/js/main.js
+++ static/js/main.js
@@ -1,32 +1,66 @@
-
-function ViewModel() {
+function BannerViewModel() {
     var self = this;
-    self.sections = ko.observableArray();
+    self.bannerList = ko.observableArray();
 
     $.ajax({
         dataType: "json",
-        url: "https://api.myjson.com/bins/1gbt6d",
+        url: "http://gae-gamma.pratilipi.com/api/init/banner/list?language=HINDI",
+        success: function(data) {
+            ko.utils.arrayPushAll(self.bannerList, data['bannerList']);
+        }
+    });
+};
+ko.applyBindings(new BannerViewModel(), document.getElementById("banners"));
+
+
+
+function SectionViewModel() {
+    var self = this;
+    self.sections = ko.observableArray();
+    //self.selected = ko.observable();
+
+    $.ajax({
+        dataType: "json",
+        url: "http://gae-gamma.pratilipi.com/api/init?language=HINDI&_apiVer=2",
         success: function(data) {
             ko.utils.arrayPushAll(self.sections, data['sections']);
         }
+        // self.selectItem = function(data) {
+        //     self.selected(data);
+        // }
     });
     ko.bindingHandlers.owlCarouselInitiator = {
-
         init: function() {
             $('.owl-carousel').owlCarousel({
               loop:true,
               nav:true,
-              responsiveClass:true,
-              center:false,
-              autoWidth:true,
+              dots:false,
+              margin:15,
               rewind:true,
               lazyLoad:true,
               animateIn:true,
               animateOut:true,
               navClass: ['prev', 'next'],
               navText:['<span type="button" class="btn fa fa-lg fa-angle-left"> </span>','<span type="button" class="btn  fa fa-lg fa-angle-right"> </span>'],
+              responsiveClass:true,
+              responsive:{
+                  0:{
+                      items:3,
+                      nav:true
+                  },
+                  600:{
+                      items:3,
+                      nav:false
+                  },
+                  1000:{
+                      items:5,
+                      nav:true,
+                      loop:false
+                  }
+              }
           })
         }
     };
 };
-ko.applyBindings(new ViewModel());
+ko.applyBindings(new SectionViewModel(), document.getElementById("sections"));
+
